19830b4b89f162a5f47ec37cc50c94de6bc9c44e
Ray Sandhu <aryamansandhu123@gmail.com>
2023-08-12 22:36:13 -0600

    Attempts at x-axis hover. Minor tweaks

---
+++ index.js
@@ -0,0 +1,21 @@
+// CODE TO TRIAL X-AXIS HIGHLIGHTING OF NUCLEOBASES
+
+// const sidebar = document.querySelector('.sidebar');
+// const nucleobases = document.querySelectorAll('.nucleobase');
+
+// if (sidebar) {
+// 	sidebar.addEventListener('mousemove', (event) => {
+// 		const x = event.clientX - sidebar.getBoundingClientRect().left;
+// 		console.log($el_size); // not a defined value
+// 		nucleobases.forEach((nucleobase, index) => {
+// 			const nucleobaseX = index * ($el_size * 1.7); //undefined
+// 			if (x >= nucleobaseX && x < nucleobaseX + $el_size) {
+// 				nucleobase.classList.add('enlarge');
+// 				console.log('Enlarging at ', x);
+// 			} else {
+// 				nucleobase.classList.remove('enlarge');
+// 			}
+// 		});
+// 		console.log('Something happened');
+// 	});
+// }
